3c3044c109f857fa9e7dcddf58302a2baf679674
Pablo Herrero <pherrero1612@ieszaidinvergeles.org>
2023-02-21 14:18:34 +0100

    Scrap

---
+++ scrap.py
@@ -0,0 +1,8 @@
+import os
+import requests
+
+r = requests.get("https://youtube.com", cookies={'CONSENT': 'YES+1'})
+print (r.text)
+f = open("c:/Users/Pablo/Desktop/pagina.txt", "w", encoding="utf-8")
+f.write(r.text)
+f.close()
